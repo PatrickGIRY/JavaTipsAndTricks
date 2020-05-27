fa1121279d3b7223cde8cc63d32c59c1f79eb4ba
Patrick GIRY <patrickgiry@gmail.com>
2020-05-27 20:33:01 +0200

    Read larger text file way 2

--- src/file/channel/README.md
+++ src/file/channel/README.md
@@ -5,4 +5,5 @@ Ways to read as text file using `FileChannel`
 * Read a file using `ByteBuffer` of file size.
 * Read a file with fixed size `ByteBuffer`.
 * Read small text file.
-* Read larger text file.
+* Read larger text file
+* Read larger text file way 2.
---
+++ src/file/channel/ReadLargerTextFileWay2.java
@@ -0,0 +1,17 @@
+package file.channel;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadLargerTextFileWay2 {
+
+    public static void main(String[] args) throws IOException {
+        var path = Paths.get("laposte_hexasmal.csv");
+        try (var reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+            reader.lines().forEach(System.out::println);
+        }
+
+    }
+}
